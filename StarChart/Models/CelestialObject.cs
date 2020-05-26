76ae5f3d846f10dc2548a4aea4c1d177b5e305c5
pklimesh <63873212+pklimesh@users.noreply.github.com>
2020-05-26 11:17:27 -0500

    StarChart5-3

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int ID;
+        public string Name; [Required]
+        public int? OrbotedObjectID;
+        public List<CelestialObject> Satellites;
+        public TimeSpan OrbitalPeriod;
+    }
+}
