bbf02b96427070d2ad9c2e89b8e6f58b9456b2fd
pklimesh <63873212+pklimesh@users.noreply.github.com>
2020-05-26 11:34:05 -0500

    StarChart5-3

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Threading.Tasks;
 
 namespace StarChart.Models
 {
