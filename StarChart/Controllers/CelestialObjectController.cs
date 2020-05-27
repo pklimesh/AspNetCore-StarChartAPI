9980db4c6fd852e126aef197e47d3fda796e98d9
pklimesh <63873212+pklimesh@users.noreply.github.com>
2020-05-27 07:11:21 -0500

    StarChart5-4

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+using System.Threading.Tasks;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController
+    {
+        private ApplicationDbContext _context;
+    }
+}
